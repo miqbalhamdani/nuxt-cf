7e15bbf8e1ffeb433e32aa0dedc13f81f782c17a
Iqbal Hamdani <miqbalh94@gmail.com>
2024-01-08 15:46:33 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
+  devtools: { enabled: true },
+  extends: [
+    'layer-flight',
+    'layer-train',
+  ],
+})
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtLink to="/flights">
+      <q-btn color="white" text-color="black" label="Flight" />
+    </NuxtLink>
+    <NuxtLink to="/trains">
+      <q-btn color="primary" label="Train" />
+    </NuxtLink>
+
+    <h1>Welcome to 2024 ...</h1>
+    <Search />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+console.log(route);
+</script>
